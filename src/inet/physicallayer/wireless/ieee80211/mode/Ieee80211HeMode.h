e2a8e505adbe6922c4f783587ee8b7b7757916f9
Miguel González López <miguel.gonzalez.lopez@udc.es>
2026-06-14 12:25:34 +0200

    GSD quickfix: replace Ieee80211ax names by Ieee80211He to maintain consistency with current codebase

--- src/inet/physicallayer/wireless/ieee80211/mode/Ieee80211HeMode.h
+++ src/inet/physicallayer/wireless/ieee80211/mode/Ieee80211HeMode.h
@@ -4,8 +4,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 //
 
-#ifndef __INET_IEEE80211AXMODE_H
-#define __INET_IEEE80211AXMODE_H
+#ifndef __INET_IEEE80211HEMODE_H
+#define __INET_IEEE80211HEMODE_H
 
 #define DI    DelayedInitializer
 
@@ -18,7 +18,7 @@
 namespace inet {
 namespace physicallayer {
 
-class INET_API Ieee80211AxTimingRelatedParametersBase
+class INET_API Ieee80211HeTimingRelatedParametersBase
 {
   public:
     const simtime_t getDFTPeriod() const { return 12.8E-6; } // DFT period for HE is 12.8 µs
@@ -28,12 +28,12 @@ class INET_API Ieee80211AxTimingRelatedParametersBase
     const simtime_t getShortGISymbolInterval() const { return getDFTPeriod() + getShortGIDuration(); } // 13.6 µs
 };
 
-class INET_API Ieee80211AxModeBase
+class INET_API Ieee80211HeModeBase
 {
   public:
     enum GuardIntervalType {
-        AX_GUARD_INTERVAL_SHORT, // 0.8 µs
-        AX_GUARD_INTERVAL_LONG   // 3.2 µs
+        HE_GUARD_INTERVAL_SHORT, // 0.8 µs
+        HE_GUARD_INTERVAL_LONG   // 3.2 µs
     };
 
   protected:
@@ -50,7 +50,7 @@ class INET_API Ieee80211AxModeBase
     virtual bps computeNetBitrate() const = 0;
 
   public:
-    Ieee80211AxModeBase(unsigned int modulationAndCodingScheme, unsigned int numberOfSpatialStreams, const Hz bandwidth, GuardIntervalType guardIntervalType);
+    Ieee80211HeModeBase(unsigned int modulationAndCodingScheme, unsigned int numberOfSpatialStreams, const Hz bandwidth, GuardIntervalType guardIntervalType);
 
     virtual int getNumberOfDataSubcarriers() const;
     virtual int getNumberOfPilotSubcarriers() const;
@@ -63,7 +63,7 @@ class INET_API Ieee80211AxModeBase
     virtual bps getGrossBitrate() const;
 };
 
-class INET_API Ieee80211AxSignalMode : public IIeee80211HeaderMode, public Ieee80211AxModeBase, public Ieee80211AxTimingRelatedParametersBase
+class INET_API Ieee80211HeSignalMode : public IIeee80211HeaderMode, public Ieee80211HeModeBase, public Ieee80211HeTimingRelatedParametersBase
 {
   protected:
     const Ieee80211OfdmModulation *modulation;
@@ -74,23 +74,23 @@ class INET_API Ieee80211AxSignalMode : public IIeee80211HeaderMode, public Ieee8
     virtual bps computeNetBitrate() const override;
 
   public:
-    Ieee80211AxSignalMode(unsigned int modulationAndCodingScheme, const Ieee80211OfdmModulation *modulation, const Ieee80211VhtCode *code, const Hz bandwidth, GuardIntervalType guardIntervalType);
-    Ieee80211AxSignalMode(unsigned int modulationAndCodingScheme, const Ieee80211OfdmModulation *modulation, const Ieee80211ConvolutionalCode *convolutionalCode, const Hz bandwidth, GuardIntervalType guardIntervalType);
-    virtual ~Ieee80211AxSignalMode();
+    Ieee80211HeSignalMode(unsigned int modulationAndCodingScheme, const Ieee80211OfdmModulation *modulation, const Ieee80211VhtCode *code, const Hz bandwidth, GuardIntervalType guardIntervalType);
+    Ieee80211HeSignalMode(unsigned int modulationAndCodingScheme, const Ieee80211OfdmModulation *modulation, const Ieee80211ConvolutionalCode *convolutionalCode, const Hz bandwidth, GuardIntervalType guardIntervalType);
+    virtual ~Ieee80211HeSignalMode();
 
     virtual unsigned int getModulationAndCodingScheme() const { return mcsIndex; }
     virtual const simtime_t getDuration() const override { return 2 * getSymbolInterval(); }
     virtual b getLength() const override { return b(48); }
-    virtual bps getNetBitrate() const override { return Ieee80211AxModeBase::getNetBitrate(); }
-    virtual bps getGrossBitrate() const override { return Ieee80211AxModeBase::getGrossBitrate(); }
-    virtual const simtime_t getSymbolInterval() const override { return Ieee80211AxTimingRelatedParametersBase::getSymbolInterval(); }
+    virtual bps getNetBitrate() const override { return Ieee80211HeModeBase::getNetBitrate(); }
+    virtual bps getGrossBitrate() const override { return Ieee80211HeModeBase::getGrossBitrate(); }
+    virtual const simtime_t getSymbolInterval() const override { return Ieee80211HeTimingRelatedParametersBase::getSymbolInterval(); }
     virtual const Ieee80211OfdmModulation *getModulation() const override { return modulation; }
     virtual const Ieee80211VhtCode *getCode() const { return code; }
 
     virtual Ptr<Ieee80211PhyHeader> createHeader() const override { return makeShared<Ieee80211HtPhyHeader>(); }
 };
 
-class INET_API Ieee80211AxPreambleMode : public IIeee80211PreambleMode, public Ieee80211AxTimingRelatedParametersBase
+class INET_API Ieee80211HePreambleMode : public IIeee80211PreambleMode, public Ieee80211HeTimingRelatedParametersBase
 {
   public:
     enum HighEfficiencyPreambleFormat {
@@ -99,7 +99,7 @@ class INET_API Ieee80211AxPreambleMode : public IIeee80211PreambleMode, public I
     };
 
   protected:
-    const Ieee80211AxSignalMode *highEfficiencySignalMode;
+    const Ieee80211HeSignalMode *highEfficiencySignalMode;
     const Ieee80211OfdmSignalMode *legacySignalMode;
     const HighEfficiencyPreambleFormat preambleFormat;
     const unsigned int numberOfHELongTrainings;
@@ -108,11 +108,11 @@ class INET_API Ieee80211AxPreambleMode : public IIeee80211PreambleMode, public I
     virtual unsigned int computeNumberOfHELongTrainings(unsigned int numberOfSpatialStreams) const;
 
   public:
-    Ieee80211AxPreambleMode(const Ieee80211AxSignalMode *highEfficiencySignalMode, const Ieee80211OfdmSignalMode *legacySignalMode, HighEfficiencyPreambleFormat preambleFormat, unsigned int numberOfSpatialStreams);
-    virtual ~Ieee80211AxPreambleMode() { delete highEfficiencySignalMode; }
+    Ieee80211HePreambleMode(const Ieee80211HeSignalMode *highEfficiencySignalMode, const Ieee80211OfdmSignalMode *legacySignalMode, HighEfficiencyPreambleFormat preambleFormat, unsigned int numberOfSpatialStreams);
+    virtual ~Ieee80211HePreambleMode() { delete highEfficiencySignalMode; }
 
     HighEfficiencyPreambleFormat getPreambleFormat() const { return preambleFormat; }
-    virtual const Ieee80211AxSignalMode *getSignalMode() const { return highEfficiencySignalMode; }
+    virtual const Ieee80211HeSignalMode *getSignalMode() const { return highEfficiencySignalMode; }
     virtual const Ieee80211OfdmSignalMode *getLegacySignalMode() const { return legacySignalMode; }
 
     virtual const simtime_t getDoubleGIDuration() const { return 2 * getGIDuration(); }
@@ -129,7 +129,7 @@ class INET_API Ieee80211AxPreambleMode : public IIeee80211PreambleMode, public I
     virtual Ptr<Ieee80211PhyPreamble> createPreamble() const override { return makeShared<Ieee80211VhtPhyPreamble>(); }
 };
 
-class INET_API Ieee80211Axmcs
+class INET_API Ieee80211Hemcs
 {
   protected:
     const unsigned int mcsIndex;
@@ -139,9 +139,9 @@ class INET_API Ieee80211Axmcs
     const Hz bandwidth;
 
   public:
-    Ieee80211Axmcs(unsigned int mcsIndex, const ApskModulationBase *stream1SubcarrierModulation, const Ieee80211ConvolutionalCode* convolutionalCode, Hz bandwidth);
-    Ieee80211Axmcs(unsigned int mcsIndex, const ApskModulationBase *stream1SubcarrierModulation, const ApskModulationBase *stream2SubcarrierModulation, const Ieee80211ConvolutionalCode* convolutionalCode, Hz bandwidth);
-    virtual ~Ieee80211Axmcs();
+    Ieee80211Hemcs(unsigned int mcsIndex, const ApskModulationBase *stream1SubcarrierModulation, const Ieee80211ConvolutionalCode* convolutionalCode, Hz bandwidth);
+    Ieee80211Hemcs(unsigned int mcsIndex, const ApskModulationBase *stream1SubcarrierModulation, const ApskModulationBase *stream2SubcarrierModulation, const Ieee80211ConvolutionalCode* convolutionalCode, Hz bandwidth);
+    virtual ~Ieee80211Hemcs();
 
     const Ieee80211VhtCode *getCode() const { return code; }
     unsigned int getMcsIndex() const { return mcsIndex; }
@@ -153,39 +153,39 @@ class INET_API Ieee80211Axmcs
     }
 };
 
-class INET_API Ieee80211AxDataMode : public IIeee80211DataMode, public Ieee80211AxModeBase, public Ieee80211AxTimingRelatedParametersBase
+class INET_API Ieee80211HeDataMode : public IIeee80211DataMode, public Ieee80211HeModeBase, public Ieee80211HeTimingRelatedParametersBase
 {
   protected:
-    const Ieee80211Axmcs *modulationAndCodingScheme;
+    const Ieee80211Hemcs *modulationAndCodingScheme;
     const unsigned int numberOfBccEncoders;
 
   protected:
     bps computeGrossBitrate() const override;
     bps computeNetBitrate() const override;
-    unsigned int computeNumberOfSpatialStreams(const Ieee80211Axmcs *) const;
+    unsigned int computeNumberOfSpatialStreams(const Ieee80211Hemcs *) const;
     unsigned int computeNumberOfCodedBitsPerSubcarrierSum() const;
     unsigned int computeNumberOfBccEncoders() const;
 
   public:
-    Ieee80211AxDataMode(const Ieee80211Axmcs *modulationAndCodingScheme, const Hz bandwidth, GuardIntervalType guardIntervalType);
+    Ieee80211HeDataMode(const Ieee80211Hemcs *modulationAndCodingScheme, const Hz bandwidth, GuardIntervalType guardIntervalType);
 
     b getServiceFieldLength() const { return b(16); }
     b getTailFieldLength() const { return b(6) * numberOfBccEncoders; }
 
-    virtual int getNumberOfSpatialStreams() const override { return Ieee80211AxModeBase::getNumberOfSpatialStreams(); }
+    virtual int getNumberOfSpatialStreams() const override { return Ieee80211HeModeBase::getNumberOfSpatialStreams(); }
     virtual Hz getBandwidth() const override { return bandwidth; }
     virtual b getPaddingLength(b dataLength) const override { return b(0); }
     virtual b getCompleteLength(b dataLength) const override;
     virtual const simtime_t getDuration(b dataLength) const override;
-    virtual bps getNetBitrate() const override { return Ieee80211AxModeBase::getNetBitrate(); }
-    virtual bps getGrossBitrate() const override { return Ieee80211AxModeBase::getGrossBitrate(); }
-    virtual const Ieee80211Axmcs *getModulationAndCodingScheme() const { return modulationAndCodingScheme; }
+    virtual bps getNetBitrate() const override { return Ieee80211HeModeBase::getNetBitrate(); }
+    virtual bps getGrossBitrate() const override { return Ieee80211HeModeBase::getGrossBitrate(); }
+    virtual const Ieee80211Hemcs *getModulationAndCodingScheme() const { return modulationAndCodingScheme; }
     virtual const Ieee80211VhtCode *getCode() const { return modulationAndCodingScheme->getCode(); }
-    virtual const simtime_t getSymbolInterval() const override { return Ieee80211AxTimingRelatedParametersBase::getSymbolInterval(); }
+    virtual const simtime_t getSymbolInterval() const override { return Ieee80211HeTimingRelatedParametersBase::getSymbolInterval(); }
     virtual const Ieee80211OfdmModulation *getModulation() const override { return modulationAndCodingScheme->getModulation(); }
 };
 
-class INET_API Ieee80211AxMode : public Ieee80211ModeBase
+class INET_API Ieee80211HeMode : public Ieee80211ModeBase
 {
   public:
     enum BandMode {
@@ -194,8 +194,8 @@ class INET_API Ieee80211AxMode : public Ieee80211ModeBase
     };
 
   protected:
-    const Ieee80211AxPreambleMode *preambleMode;
-    const Ieee80211AxDataMode *dataMode;
+    const Ieee80211HePreambleMode *preambleMode;
+    const Ieee80211HeDataMode *dataMode;
     const BandMode centerFrequencyMode;
 
   protected:
@@ -203,12 +203,12 @@ class INET_API Ieee80211AxMode : public Ieee80211ModeBase
     virtual int getLegacyCwMax() const override { return 1023; }
 
   public:
-    Ieee80211AxMode(const char *name, const Ieee80211AxPreambleMode *preambleMode, const Ieee80211AxDataMode *dataMode, const BandMode centerFrequencyMode);
-    virtual ~Ieee80211AxMode() { delete preambleMode; delete dataMode; }
+    Ieee80211HeMode(const char *name, const Ieee80211HePreambleMode *preambleMode, const Ieee80211HeDataMode *dataMode, const BandMode centerFrequencyMode);
+    virtual ~Ieee80211HeMode() { delete preambleMode; delete dataMode; }
 
-    virtual const Ieee80211AxDataMode *getDataMode() const override { return dataMode; }
-    virtual const Ieee80211AxPreambleMode *getPreambleMode() const override { return preambleMode; }
-    virtual const Ieee80211AxSignalMode *getHeaderMode() const override { return preambleMode->getSignalMode(); }
+    virtual const Ieee80211HeDataMode *getDataMode() const override { return dataMode; }
+    virtual const Ieee80211HePreambleMode *getPreambleMode() const override { return preambleMode; }
+    virtual const Ieee80211HeSignalMode *getHeaderMode() const override { return preambleMode->getSignalMode(); }
     virtual const Ieee80211OfdmSignalMode *getLegacySignalMode() const { return preambleMode->getLegacySignalMode(); }
 
     virtual const simtime_t getSlotTime() const override;
@@ -225,48 +225,48 @@ class INET_API Ieee80211AxMode : public Ieee80211ModeBase
     virtual const simtime_t getDuration(b dataBitLength) const override { return preambleMode->getDuration() + dataMode->getDuration(dataBitLength); }
 };
 
-class INET_API Ieee80211AxmcsTable
+class INET_API Ieee80211HemcsTable
 {
   public:
-    static const DI<Ieee80211Axmcs> axMcs0BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs1BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs2BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs3BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs4BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs5BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs6BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs7BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs8BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs9BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs10BW20MHzNss1;
-    static const DI<Ieee80211Axmcs> axMcs11BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs0BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs1BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs2BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs3BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs4BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs5BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs6BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs7BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs8BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs9BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs10BW20MHzNss1;
+    static const DI<Ieee80211Hemcs> heMcs11BW20MHzNss1;
 
-    static const DI<Ieee80211Axmcs> axMcs0BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs1BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs2BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs3BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs4BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs5BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs6BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs7BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs8BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs9BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs10BW20MHzNss2;
-    static const DI<Ieee80211Axmcs> axMcs11BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs0BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs1BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs2BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs3BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs4BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs5BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs6BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs7BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs8BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs9BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs10BW20MHzNss2;
+    static const DI<Ieee80211Hemcs> heMcs11BW20MHzNss2;
 };
 
-class INET_API Ieee80211AxCompliantModes
+class INET_API Ieee80211HeCompliantModes
 {
   protected:
-    static OPP_THREAD_LOCAL const Ieee80211AxCompliantModes singleton;
+    static OPP_THREAD_LOCAL const Ieee80211HeCompliantModes singleton;
 
-    mutable std::map<std::tuple<Hz, unsigned int, Ieee80211AxModeBase::GuardIntervalType, unsigned int>, const Ieee80211AxMode *> modeCache;
+    mutable std::map<std::tuple<Hz, unsigned int, Ieee80211HeModeBase::GuardIntervalType, unsigned int>, const Ieee80211HeMode *> modeCache;
 
   public:
-    Ieee80211AxCompliantModes();
-    virtual ~Ieee80211AxCompliantModes();
+    Ieee80211HeCompliantModes();
+    virtual ~Ieee80211HeCompliantModes();
 
-    static const Ieee80211AxMode *getCompliantMode(const Ieee80211Axmcs *mcsMode, Ieee80211AxMode::BandMode centerFrequencyMode, Ieee80211AxPreambleMode::HighEfficiencyPreambleFormat preambleFormat, Ieee80211AxModeBase::GuardIntervalType guardIntervalType);
+    static const Ieee80211HeMode *getCompliantMode(const Ieee80211Hemcs *mcsMode, Ieee80211HeMode::BandMode centerFrequencyMode, Ieee80211HePreambleMode::HighEfficiencyPreambleFormat preambleFormat, Ieee80211HeModeBase::GuardIntervalType guardIntervalType);
 };
 
 } // namespace physicallayer
