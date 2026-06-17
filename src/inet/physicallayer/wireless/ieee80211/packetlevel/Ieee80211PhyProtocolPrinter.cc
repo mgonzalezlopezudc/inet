81ebde9172de9e392c194a778881a6163c4c598d
Miguel González López <miguel.gonzalez.lopez@udc.es>
2026-06-17 23:00:28 +0200

    feat: improve visualization of HE DL MU PPDUs in the simulation window

--- src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyProtocolPrinter.cc
+++ src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyProtocolPrinter.cc
@@ -7,6 +7,8 @@
 
 #include "inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyProtocolPrinter.h"
 
+#include <sstream>
+
 #include "inet/common/packet/printer/PacketPrinter.h"
 #include "inet/common/packet/printer/ProtocolPrinterRegistry.h"
 #include "inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader_m.h"
@@ -27,7 +29,25 @@ Register_Protocol_Printer(&Protocol::ieee80211HePhy, Ieee80211PhyProtocolPrinter
 void Ieee80211PhyProtocolPrinter::print(const Ptr<const Chunk>& chunk, const Protocol *protocol, const cMessagePrinter::Options *options, Context& context) const
 {
     if (auto heMuHeader = dynamicPtrCast<const Ieee80211HeMuPhyHeader>(chunk)) {
-        context.infoColumn << "HE MU PHY, " << heMuHeader->getUsersArraySize() << " users";
+        constexpr size_t maxPrintedUsers = 6;
+        auto usersCount = heMuHeader->getUsersArraySize();
+        std::ostringstream stream;
+        stream << "HE MU PHY, " << usersCount << " users";
+        if (usersCount > 0)
+            stream << ": ";
+        size_t shownUsers = std::min(maxPrintedUsers, usersCount);
+        for (size_t i = 0; i < shownUsers; i++) {
+            if (i != 0)
+                stream << "; ";
+            const auto& user = heMuHeader->getUsers(i);
+            stream << "RU" << user.ruIndex << "->STA" << user.staId
+                   << " (MCS" << static_cast<int>(user.mcs)
+                   << ", NSS" << static_cast<int>(user.numberOfSpatialStreams)
+                   << ", DCM" << (user.dcm ? 1 : 0) << ")";
+        }
+        if (usersCount > maxPrintedUsers)
+            stream << "; +" << (usersCount - maxPrintedUsers) << " more";
+        context.infoColumn << stream.str();
     }
     else {
         context.infoColumn << "(IEEE 802.11 Phy) " << chunk;
