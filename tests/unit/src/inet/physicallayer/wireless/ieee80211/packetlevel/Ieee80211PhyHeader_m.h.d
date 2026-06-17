2a4ce2ff12ae08fb5d0aaa9660a8cab7ecf4f0c0
Miguel González López <miguel.gonzalez.lopez@udc.es>
2026-06-17 20:54:58 +0200

    chore: various cleaning tasks

---
+++ tests/unit/src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader_m.h.d
@@ -0,0 +1,13 @@
+../../../../src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader_m.cc ../../../../src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader_m.h : \
+	../../../../src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader.msg \
+	../../../../src/inet/common/INETDefs.msg \
+	../../../../src/inet/common/Units.msg \
+	../../../../src/inet/common/checksum/ChecksumMode.msg \
+	../../../../src/inet/common/packet/chunk/Chunk.msg \
+	../../../../src/inet/linklayer/common/FcsMode.msg
+../../../../src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader.msg:
+../../../../src/inet/common/INETDefs.msg:
+../../../../src/inet/common/Units.msg:
+../../../../src/inet/common/checksum/ChecksumMode.msg:
+../../../../src/inet/common/packet/chunk/Chunk.msg:
+../../../../src/inet/linklayer/common/FcsMode.msg:
